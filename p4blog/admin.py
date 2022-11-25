8b7a0fd381289b9593e8c4016b0003c3083a198a
Pegrume88 <pegrume88@gmail.com>
2022-11-25 13:50:14 +0000

    added prepopulated slug field the postadmin class

--- p4blog/admin.py
+++ p4blog/admin.py
@@ -6,5 +6,6 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = 'content'
 
