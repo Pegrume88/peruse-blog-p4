a9a96f77154d19a0b1a29081370988017bc50c84
Pegrume88 <pegrume88@gmail.com>
2022-11-25 13:46:20 +0000

    add summernote and register it in urls.py and set up superuser and admin page

--- peruse/settings.py
+++ peruse/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'p4blog',
 ]
 
@@ -138,7 +139,7 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'coudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
