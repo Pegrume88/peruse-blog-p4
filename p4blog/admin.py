b041c71102d1b257338b7247f0bbd39c7c78ec8a
Pegrume88 <pegrume88@gmail.com>
2022-11-25 13:53:09 +0000

    added filter box to admin page that allows user to filter by status and created date

--- p4blog/admin.py
+++ p4blog/admin.py
@@ -7,5 +7,6 @@ from django_summernote.admin import SummernoteModelAdmin
 class PostAdmin(SummernoteModelAdmin):
 
     prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = 'content'
 
