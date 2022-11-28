4155c1985979db0cb255062095edca5779958ccb
Pegrume88 <pegrume88@gmail.com>
2022-11-28 14:37:42 +0000

    blog post code added to index.html, urls.py created in p4 blog with path added, css styling added to index.html

--- peruse/urls.py
+++ peruse/urls.py
@@ -18,9 +18,9 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    
-    path('admin/', admin.site.urls),
 
+    path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    
+    path('', include('p4blog.urls'), name='p4blog_urls')
+
  ]
