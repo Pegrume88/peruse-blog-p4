beea9ef575f76a83881a558c3d9a624c42cf0e6d
Pegrume88 <pegrume88@gmail.com>
2022-11-28 19:47:47 +0000

    fix css connection issue and reset data base to solve error

--- p4blog/models.py
+++ p4blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=220, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="p4blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
--- p4blog/views.py
+++ p4blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
@@ -10,5 +10,24 @@ class PostList(generic.ListView):
     paginate_by = 5
 
 
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
 
 
