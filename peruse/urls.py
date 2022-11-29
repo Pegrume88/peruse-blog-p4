a09552214e8fe6d741216be3de9fddbfbad3f2d4
Pegrume88 <pegrume88@gmail.com>
2022-11-29 09:16:58 +0000

    installed django authentications and added login and lohout redirects to home page

--- peruse/urls.py
+++ peruse/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('p4blog.urls'), name='p4blog_urls'),
+    path('accounts/', include('allauth.urls')),
 
  ]
