808833661aa6b3d89b02d964b48f4646f29b2354
Pegrume88 <pegrume88@gmail.com>
2022-12-08 09:54:44 +0000

    added JS to automaticallu assign correct author and user id to posts

--- p4blog/forms.py
+++ p4blog/forms.py
@@ -18,7 +18,8 @@ class PostForm(forms.ModelForm):
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'user', 'type': 'hidden'}),
+            # 'author': forms.Select(attrs={'class': 'form-control'}),
             'category': forms.Select(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
             'featured_image': forms.FileInput(attrs={'class': 'form-control-file'}),
