6ab25bd05c90bffb661c903052404a8760220df3
Pegrume88 <pegrume88@gmail.com>
2022-12-10 17:17:46 +0000

    form validation test completed

--- p4blog/tests.py
+++ p4blog/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
 from .forms import PostForm
-
+from pathlib import Path
+from .models import User, Category
 
 """
 Testing forms 
 """
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 class TestPostForm(TestCase):
 
@@ -15,18 +18,23 @@ class TestPostForm(TestCase):
         """
         Test if form submits without title field
         """
-
+        upload_file = open('p4blog/urls.py', 'rb')
+        author = User(username="test", password="test12345")
+        author.save()
+        category = Category(name="test")
+        category.save()
         form = PostForm({
-            'title': '',
-            'author': 'test',
-            'category': 'test',
+            'author': author,
+            'category': category,
             'content': 'test',
-            'featured_image': 'test',
+            'featured_image': SimpleUploadedFile(upload_file.name, upload_file.read()),
 
         })
         # Form should not be valid - title required
+        
         self.assertFalse(form.is_valid())
-        #self.assertIn('title', form.errors.keys())
+        
+        self.assertIn('title', form.errors.keys())
         # Check error message is correct
-        #self.assertEqual(
-           # form.errors['title'][0], 'This field is required.')
+        self.assertEqual(
+            form.errors['title'][0], 'This field is required.')
