f592eec2365701a0af5ffd30c0642b4dd420a130
Pegrume88 <pegrume88@gmail.com>
2022-12-13 18:35:24 +0000

    adjusted allowed host to check if it deploy correctly

--- peruse/settings.py
+++ peruse/settings.py
@@ -24,11 +24,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 # Add Render.com URL to allowed hosts
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('https://peruse-p4-blog.onrender.com')
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append('https://peruse-p4-blog.onrender.com')
-
-
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 # Application definition
 
 INSTALLED_APPS = [
